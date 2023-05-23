d2ee241502fd976924b40c329ed1ce900937545a
kimskkkkky <ses513a@gmail.com>
2023-05-23 13:14:09 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "haneul kim";
+        System.out.println(name);
+        // return 0;
+    }
+}
---
+++ src/Prints.java
@@ -0,0 +1,6 @@
+public class Prints {
+    public static void main(String[] args) {
+        String title = "빅데이터 java 교육 과정";
+        System.out.println(title);
+    }
+}

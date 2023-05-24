71208cb71b5b81e9f91fe9fd8116ddb8b1c0077c
kimskkkkky <ses513a@gmail.com>
2023-05-24 13:10:17 +0900

    scanner 백준풀이

---
+++ src/plusTwoVar.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class plusTwoVar {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int first = myObj.nextInt();
+        int second = myObj.nextInt();
+        System.out.println(first+second);
+    }
+    
+}

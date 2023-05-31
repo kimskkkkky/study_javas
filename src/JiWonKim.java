e8a3196bcd1bc4839ecd834ea64d78e812514dc1
ppquokka <quokka2335@gmail.com>
2023-05-31 12:20:22 +0900

    공유 파일 연습 포함

---
+++ src/JiWonKim.java
@@ -0,0 +1,10 @@
+public class JiWonKim {
+    public static void main(String[] args) {
+        try {
+            System.out.println("안녕하세요:)");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+}

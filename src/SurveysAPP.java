d6dfabe7c238360fde0268aee9cf31912660fb1e
AKAVespaET3 <quattrini244@gmail.com>
2023-06-15 18:23:21 +0900

    2023-06-15

--- src/SurveysAPP.java
+++ src/SurveysAPP.java
@@ -8,7 +8,7 @@ import surveys.Statistics;
 public class SurveysAPP {
     public static void main(String[] args) {
         try {
-            
+
             // - MySQL workbench 실행 : JDBC
             // - User/password와 접속 IP:port 접속
             String url = "jdbc:mysql://127.0.0.1:3306/db_survey";
@@ -20,12 +20,52 @@ public class SurveysAPP {
 
             // - query Edit
             Statement statement = connection.createStatement();
+            System.out.println("-----설문자 가능 명단-----");
+            // -- - 설문자 가능 명단(가입 완료)
+            // -- 1. 홍길동, 2.장길산, 3.신사임당, ...
+            String queryA = "SELECT respondents.RESPONDENTS\n" + //
+                    "FROM respondents";
+            ResultSet resultSet = statement.executeQuery(queryA);
+            int number = 1;
+            while (resultSet.next()) {
+                System.out.print(number + ". " + resultSet.getString("RESPONDENTS") + ",");
+                number = number + 1;
+            }
+            System.out.println();
+            System.out.println();
+            // -- 설문 시작
+            // -- 1. 교수는 수업 전 강의 목표를 명확히 제시하였습니까?
+            // -- 문항 먼저 디스플레이
+            System.out.println("----- 설문 시작 -----");
+            queryA = "SELECT *\n" + //
+                    "FROM questions";
+            resultSet = statement.executeQuery(queryA);
+            Statement statement_second = connection.createStatement();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("questions"));
+
+                queryA = "SELECT T_CHO.CHOICE_ID, T_CHO.CHOICE\n" + //
+                        "FROM question_choice AS T_QUES\n" + //
+                        "INNER JOIN choice AS T_CHO\n" + //
+                        "ON T_QUES.CHOICE_ID = T_CHO.CHOICE_ID\n" + //
+                        "AND QUESTIONS_ID = 'Q1' \n";
+                        
+                ResultSet resultSet_second = statement_second.executeQuery(queryA);
+                int number_second = 1;
+                while (resultSet_second.next()) {
+                    System.out.print(number_second + ". " + resultSet_second.getString("T_CHO.CHOICE") + " ");
+                    number_second = number_second+1;
+                }
+                System.out.println();
+                System.out.println();
+            }
+            System.out.println();
 
             // 통계 - 총 설문자 표시
             Statistics statistics = new Statistics();
             statistics.getRespondents(statement);
-            }catch (Exception e) {
+        } catch (Exception e) {
             // TODO: handle exception
-        }    
+        }
     }
 }
--- src/surveys/Statistics.java
+++ src/surveys/Statistics.java
@@ -7,6 +7,7 @@ public class Statistics {
     public int getRespondents(Statement statement){
      try {
         System.out.println("-----통계-----");
+        System.out.println();
             // -- 총 설문자 3명
             String queryA = "SELECT count(*) AS CNT\n" + //
                             "FROM (SELECT RESPONDENTS_ID, count(*)\n" + //
@@ -14,6 +15,7 @@ public class Statistics {
                             "GROUP BY RESPONDENTS_ID) AS T_STATIC\n";
             ResultSet resultSet = statement.executeQuery(queryA);
             while (resultSet.next()) {
+             
                 System.out.println("-- 총 설문자" + resultSet.getString("CNT"));
              }
 
